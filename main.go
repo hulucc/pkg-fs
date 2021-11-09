87ee5b5ec1a0d573f2f6c4c28e3a3780db618aa3
hulucc <hulucc@outlook.com>
2021-11-10 00:50:44 +0800

    feat: write all

--- main.go
+++ main.go
@@ -9,7 +9,18 @@ import (
 	"strings"
 	"strconv"
 	"encoding/json"
+	"path"
+	"log"
 )
+
+func mode(jsmode int) int {
+	if i32, err := strconv.ParseInt("777", 8, 32); err == nil {
+		if result, err := strconv.ParseInt(strconv.FormatInt(int64(jsmode) & i32, 8), 10, 32); err == nil {
+			return int(result)
+		}
+	}
+	return 0
+}
 
 func search(rd *bufio.Reader, pattern string) error {
 	for {
@@ -109,6 +120,21 @@ func readBracket(rd *bufio.Reader, left string, right string) ([]byte, error) {
 	return result.Bytes(), nil
 }
 
+type StoreType string
+const StoreBlob StoreType = "0"
+const StoreContent StoreType = "1"
+const StoreLinks StoreType = "2"
+const StoreStat StoreType = "3"
+
+type FileStat struct {
+	Mode int
+	Size int64
+	IsFileValue bool
+	IsDirectoryValue bool
+	IsSocketValue bool
+	IsSymbolicLinkValue bool
+}
+
 type PkgFS struct {
 	*os.File
 	rd *bufio.Reader
@@ -116,7 +142,7 @@ type PkgFS struct {
 	PayloadSize int64
 	PreludePosition int64
 	PreludeSize int64
-	VirtualFS map[string]map[string][]int64
+	VirtualFS map[string]map[StoreType][]int64
 	Entrypoint string
 }
 
@@ -179,6 +205,84 @@ func (it *PkgFS) Initialize() error {
 	return nil
 }
 
+func (it *PkgFS) GetStat(vpath string) (*FileStat, error) {
+	f, ok := it.VirtualFS[vpath]
+	if !ok {
+		return nil, fmt.Errorf("vpath not found: %s", vpath)
+	}
+	pos := f[StoreStat][0]
+	size := f[StoreStat][1]
+	if err := it.ResetReader(it.PayloadPosition+pos); err != nil {
+		return nil, fmt.Errorf("reset header err: %w", err)
+	}
+	bs, err := it.rd.Peek(int(size))
+	if err != nil {
+		return nil, fmt.Errorf("peek err: %w", err)
+	}
+	var stat FileStat
+	if err := json.Unmarshal(bs, &stat); err != nil {
+		return nil, fmt.Errorf("json.unmarshal err: %w", err)
+	}
+	return &stat, nil
+}
+
+func (it *PkgFS) GetFile(vpath string, wr io.Writer) error {
+	f, ok := it.VirtualFS[vpath]
+	if !ok {
+		return fmt.Errorf("vpath not found: %s", vpath)
+	}
+	var pos int64
+	var size int64
+	if blob, ok := f[StoreBlob]; ok {
+		pos = blob[0]
+		size = blob[1]
+	} else if content, ok := f[StoreContent]; ok {
+		pos = content[0]
+		size = content[1]
+	} else {
+		return fmt.Errorf("no blob/content found in vpath %s", vpath)
+	}
+	if err := it.ResetReader(it.PayloadPosition+pos); err != nil {
+		return fmt.Errorf("reset reader err: %w", err)
+	}
+	if _, err := io.CopyN(wr, it.rd, size); err != nil {
+		return fmt.Errorf("io.copyn err: %w", err)
+	}
+	return nil
+}
+
+func (it *PkgFS) WriteFile(mountPoint string, vpath string) error {
+	fpath := path.Join(mountPoint, vpath)
+	os.MkdirAll(path.Dir(fpath), 0700)
+	f, err := os.Create(fpath)
+	if err != nil {
+		return fmt.Errorf("os.create(%s) err: %w", fpath, err)
+	}
+	if err := it.GetFile(vpath, f); err != nil {
+		return fmt.Errorf("GetFile err: %w", err)
+	}
+	return nil
+}
+
+func (it *PkgFS) WriteAll(mountPoint string) {
+	total := len(it.VirtualFS)
+	i := 0
+	for vpath := range it.VirtualFS {
+		stat, err := it.GetStat(vpath)
+		if err == nil {
+			if stat.IsFileValue {
+				if err := it.WriteFile(mountPoint, vpath); err != nil {
+					log.Println("write %s err: ", err)
+				}
+			}
+		} else {
+			log.Println("GetStat(%s) err: ", vpath, err)
+		}
+		fmt.Printf("\rProgress: %d/%d", i, total)
+		i++
+	}
+}
+
 func NewPkgFS(path string) (*PkgFS, error) {
 	f, err := os.Open(path)
 	if err != nil {
@@ -195,5 +299,6 @@ func main() {
 	if err := fs.Initialize(); err != nil {
 		panic(err)
 	}
-	fmt.Printf("%+v\n", fs.VirtualFS[fs.Entrypoint])
+	mountPoint := "d:\\project\\temp\\retool\\retool_backend_unpkg"
+	fs.WriteAll(mountPoint)
 }
